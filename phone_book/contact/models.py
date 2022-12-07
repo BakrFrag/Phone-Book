72476c3d64db4044cf66705d52566d0868886166
bakrfrag <bakr.frag@gmail.com>
2022-12-07 09:57:10 +0200

    add models for contact app

--- phone_book/contact/models.py
+++ phone_book/contact/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class Contact(models.Model):
+    """
+    reprsents contact name , contact name unique
+    """
+    username = models.CharField(max_length=256 , unique=True)
+
+
+class PhoneNumber(models.Model):
+    number = models.CharField(max_length=256,unique=True,validators=[
+        RegexValidator(
+       
+            regex='^01\d{9}' , 
+            message="Phone Numbers Must Start With 01 & all Are Numbers",
+        ),
+    ])
+    contact = models.ForeignKey(Contact , on_delete= models.CASCADE , related_name="numbers")
+    
+
+
