922765d4bcf8ae1102cbfc86936b63910e815b46
bakrfrag <bakr.frag@gmail.com>
2022-12-07 17:36:04 +0200

    include migrations

--- phone_book/contact/models.py
+++ phone_book/contact/models.py
@@ -6,7 +6,7 @@ class Contact(models.Model):
     """
     reprsents contact name , contact name unique
     """
-    username = models.CharField(max_length=255 , unique=True , blank=False)
+    username = models.CharField(max_length=255 , null=False , blank=False)
     def __str__(self):
         return self.username
 
